16f5c4bea446c09e108f625dc2dc0869464105f6
Xiks <xiks@mail.com>
2023-02-22 13:14:15 -0800

    First Push

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,11 @@
 #include "main.h"
 /**
- * print_alphabet-Prints the alphabet in lowercase.
- *
- * Return: Always 0 (Success)
- */
+ * print_alphabet - print all alphabet in lowercase
+*/
 void print_alphabet(void)
 {
 	char letter;
-
-
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
-		_putchar('\n');
+	_putchar('\n');
 }
-
