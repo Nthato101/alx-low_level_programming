5b686794390d8819b1b2f93b23f2a5cedb989e09
Xiks <xiks@mail.com>
2023-03-14 11:15:10 -0700

    First Push

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,8 +13,6 @@ char *str_concat(char *s1, char *s2)
 	char *conct;
 	int i, ci;
 
-
-
 	if (s1 == NULL)
 
 		s1 = "";
@@ -22,7 +20,6 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 
 		s2 = "";
-
 
 		i = ci = 0;
 
@@ -35,21 +32,16 @@ char *str_concat(char *s1, char *s2)
 
 	conct = malloc(sizeof(char) * (i + ci + 1));
 
-
 	if (conct == NULL)
 		return (NULL);
-
 	i = ci = 0;
 	while (s1[i] != '\0')
 	{
 		conct[i] = s1[i];
-		
 		i++;
 	}
-
 	while (s2[ci] != '\0')
 	{
-		
 		conct[i] = s2[ci];
 
 		i++, ci++;
