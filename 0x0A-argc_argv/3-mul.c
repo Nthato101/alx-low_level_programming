4200016135cf52ad7a0418e4b570c480ab1a3cae
Xiks <xiks@mail.com>
2023-03-14 09:47:30 -0700

    Third Push

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 #include "main.h"
-
-
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
  *
  * Return: the int converted from the string
  */
-
 int _atoi(char *s)
-
 {
 	int i, d, n, len, f, digit;
 
-
 	i = 0;
+
 	d = 0;
 
 	n = 0;
@@ -26,30 +22,20 @@ int _atoi(char *s)
 
 	digit = 0;
 
-
-
 	while (s[len] != '\0')
-
 		len++;
-
 	while (i < len && f == 0)
+
 	{
 		if (s[i] == '-')
-
 			++d;
 
-
 		if (s[i] >= '0' && s[i] <= '9')
-
 		{
 			digit = s[i] - '0';
-
 			if (d % 2)
-
 				digit = -digit;
-
 			n = n * 10 + digit;
-
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
@@ -57,10 +43,8 @@ int _atoi(char *s)
 		}
 		i++;
 	}
-
 	if (f == 0)
 		return (0);
-
 	return (n);
 }
 
